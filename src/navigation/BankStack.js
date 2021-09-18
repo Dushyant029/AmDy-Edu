0ed000ca95bd4d789129874e45a45e31e60a9893
K. Dushyant Reddy <dushireddy291@gmail.com>
2021-09-18 23:41:38 +0530

    added bankstack

---
+++ src/navigation/BankStack.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import BanksScreen from "../screen/bank";
+
+const BankStack = ({ params }) => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="BanksScreen"
+        component={BanksScreen}
+        options={{headerShown:false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default BankStack;
