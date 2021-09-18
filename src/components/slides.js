ad90205640436e9e2b48ba7e4cd9616d01790e56
K. Dushyant Reddy <dushireddy291@gmail.com>
2021-09-18 23:08:19 +0530

    swiper data

---
+++ src/components/slides.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        image: require("../../assets/images/bac1.png"),
+        desc: 'Make learning Eazy-pizy'
+    },
+    {
+        id: 2,
+        image: require("../../assets/images/bac2.png"),
+        desc: 'Join our AmDy Community'
+    },
+    {
+        id: 3,
+        image: require("../../assets/images/bac3.png"),
+        desc: 'E-learning made Convenient'
+    }
+]
